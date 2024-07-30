3f16dedd082d82c3dd1e3fb784829d632fcf9d42
PedroJGarcia <dnse.garcia@gmail.com>
2024-07-30 19:23:41 -0400

    actulizacion suma()

--- 01_consulta/11_sum.sql
+++ 01_consulta/11_sum.sql
@@ -1,5 +1,27 @@
 -- SUM(): devuelve la suma total de una columna numérica.
+SELECT SUM(precio) 
+FROM producto;
 
+-- Sintaxis:
+SELECT SUM(column_name)
+FROM table_name
+WHERE condition;
 
+-- Agregar una cláusula WHERE
+SELECT SUM(precio) AS 'Suma de precio'
+FROM producto WHERE producto_id
+
+-- Utilice un alias
+-- Dale un nombre a la columna resumida utilizando la AS palabra clave.
 SELECT SUM(precio) AS 'Suma de precio'
 FROM producto;
+
+-- Utilice SUM() con GROUP BY
+-- Aquí usamos la SUM()función y la GROUP BY
+SELECT producto_nombre,SUM(precio) AS 'Suma de precio'
+FROM producto  group by producto_nombre
+
+-- SUM() con una expresión
+-- El parámetro dentro de la SUM()función también puede ser una expresión. Como 20
+SELECT producto_nombre,SUM(precio*10) AS 'Suma de precio'
+FROM producto  group by producto_nombre
