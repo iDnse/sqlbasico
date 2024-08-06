4f8c854848a4510be7a3b802fe1f5f77f755cc56
PedroJGarcia <dnse.garcia@gmail.com>
2024-08-05 22:19:43 -0400

    host

---
+++ 02_base_de_datos/19_host.sql
@@ -0,0 +1,16 @@
+/* Hospedaje SQL
+Si desea que su sitio web pueda almacenar y recuperar datos de una base de datos,
+su servidor web debe tener acceso a un sistema de base de datos que utilice el lenguaje SQL.
+
+Si su servidor web está alojado por un proveedor de servicios de Internet 
+(ISP), tendrá que buscar planes de alojamiento SQL.
+
+Las bases de datos de alojamiento SQL más comunes son MS SQL Server, 
+Oracle, MySQL y MS Access.*/
+
+/* MySQL
+MySQL también es un software de base de datos popular para sitios web.
+
+MySQL es un sistema de base de datos SQL muy potente, robusto y completo.
+
+MySQL es una alternativa económica a las costosas soluciones de Microsoft y Oracle. */
